be4a2e0eb6c507059dcd826c70e19cb47425fbca
Krytoi95 <krytoiskelet95@gmail.com>
2025-07-25 13:42:56 +0200

    Добавлены методы класса Меню, также испытал запись и считывание обьекта из файла. Может через время более менее сделаю нормально, а потом добавлю сортировку

--- Students/Students.cpp
+++ Students/Students.cpp
@@ -10,7 +10,60 @@
 // and possibly a list of states (students, graduates, enrolling).
 // I chose students because it is a more realistic task
 
-class Menu {};
+class Menu 
+{
+private:
+    std::string Greeting{
+        "Hi! This is a regular project to practice some topics.\n"
+        "It's made in C++ on OS Windows.\n"
+        "This project is to record students in a file.\n"
+        "Are they in school, are they entering school or are they graduates.\n"
+        "Also their first name, last name, how old they are, their id.\n"
+        "And that's about it.\n"
+    };
+    std::string Enter_Student{
+        "Enter student\n"
+    };
+    std::string Enter_Name{
+    "Enter name: \n"
+    };
+    std::string Enter_Last_Name{
+        "Enter last name: \n"
+    };
+    std::string Enter_Classroom{
+        "Enter classroom: \n"
+    };
+    std::string Enter_Age{
+        "Enter age: \n"
+    };
+
+public:
+    void Say_Greeting()
+    {
+        std::cout << Greeting << std::endl;
+    }
+    void Say_Enter_Students()
+    {
+        std::cout << Enter_Student << std::endl;
+    }
+    void Say_Enter_Name()
+    {
+        std::cout << Enter_Name << std::endl;
+    }
+    void Say_Enter_Last_Name() 
+    {
+        std::cout << Enter_Last_Name << std::endl;
+    }
+    void Say_Enter_Classroom()
+    {
+        std::cout << Enter_Classroom << std::endl;
+    }
+    void Say_Enter_Age()
+    {
+        std::cout << Enter_Age << std::endl;
+    } 
+
+};
 
 class Sort {};
 
@@ -32,7 +85,7 @@ public:
         this->age = age;
         id++;
     }
-    void Info()
+    void info()
     {
         std::cout << "F: " << first_name << " S: " << second_name << " CL: " << classroom << " A: " << age  << " I: " << id << std::endl;
     }
@@ -52,6 +105,24 @@ bool write(std::fstream &file)
 
 int main()
 {
+    Menu menu;
+    std::string first_name{};
+    std::string second_name{};
+    std::string classroom{};
+    int age{};
+
+    std::cout << "Enter Student: \nSecond name: \nClassroom: \nAge: " << std::endl;
+    std::cout << "\nFirst name: " << std::endl;
+    std::cin >> first_name;
+    std::cout << "\nSecond name: " << std::endl;
+    std::cin >> second_name; 
+    std::cout << "\nClassroom: " << std::endl;
+    std::cin >> classroom;
+    std::cout << "\nAge: " << std::endl;
+    std::cin >> age;
+    Students student1(first_name, second_name, classroom, age);
+    student1.info();
+
     // Test array 
 
    /* const size_t SIZE{ 5 };
@@ -66,16 +137,26 @@ int main()
 
     // Test write\read object in file
 
-    std::fstream Write_File;
-    Write_File.open("Students.txt", std::fstream::out);
+    std::ofstream Write_File;
+    Write_File.open("Students.txt", std::fstream::app);
     try
     {
-        write(Write_File);
-        std::cout << "Correct!" << std::endl;
+        Write_File.write((char*)&student1, sizeof(Students));
+        Write_File.close();
+        std::ifstream Read_File;
+        Read_File.open("Students.txt");
+        Students student_temp;
+        while (Read_File.read((char*)&student_temp, sizeof(Students)))
+        {
+            student_temp.info();
+        }
+        Read_File.close();
     }
     catch (const std::exception& ex)
     {
         std::cout << ex.what() << std::endl;
     }
+
+    return 0;
 }
 
